5c81179dc8b91fc4bffa9b801f6cfca41dbdab34
Olha_Shutylieva <olya.shutileva@gmail.com>
2023-08-22 18:15:00 +0300

    added edu01

---
+++ src/edu01/examples/Example.java
@@ -0,0 +1,17 @@
+package edu01.examples;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.print("Hello ");
+        System.out.print("world!");
+        System.out.println("I study");
+        System.out.println("java");
+        int a = 5;
+        double b = 7;
+        int c = (int) 10.5;
+        char d = 'd';
+        System.out.println((char) (d - 50));
+    }
+
+
+}
---
+++ src/edu01/practical/ArithmeticOperations.java
@@ -0,0 +1,33 @@
+package edu01.practical;
+
+import java.util.Scanner;
+
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter value of a");
+        int a = scanner.nextInt();
+
+        System.out.println("Enter value of b");
+        int b = scanner.nextInt();
+
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        // double div = (double) a / b;
+        if (b != 0) {
+            double div1 = (double) a / b;
+            System.out.println("Result for division is " + div1);
+        } else {
+            System.out.println("Div by zero not allowed");
+        }
+
+        System.out.println("Result of Sum is " + sum);
+        System.out.println("Result of difference is " + difference);
+        System.out.println("Result of product is " + product);
+
+
+        scanner.close();
+    }
+}
