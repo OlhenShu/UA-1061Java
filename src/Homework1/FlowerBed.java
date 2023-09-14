a9a67827d52e0803df0b790dd110912d44325e0a
Yaroslav <yaroslav@mail,com>
2023-09-14 20:28:41 +0300

    Homework1

--- src/Homework1/FlowerBed.java
+++ src/Homework1/FlowerBed.java
@@ -1,3 +1,20 @@
+package Homework1;
+import java.util.Scanner;
 public class FlowerBed {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the radius of the circular flower bed ");
+
+        double radius = scanner.nextDouble();
+
+        double perimeter = 2 * Math.PI * radius;
+        double square = Math.PI * Math.pow(radius, 2);
+
+        System.out.println("The Perimeter of the flowed bed: " + perimeter);
+        System.out.println("The Square of the flowed bed: " + square);
+
+        scanner.close();
+    }
 }
 
--- src/Homework1/PhoneCalls.java
+++ src/Homework1/PhoneCalls.java
@@ -1,4 +1,41 @@
 package Homework1;
 
+import java.util.Scanner;
+
 public class PhoneCalls {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the cost per minute for the first call (c1): ");
+        double c1 = scanner.nextDouble();
+        System.out.print("Enter the duration of the first call in minutes (t1): ");
+        double t1 = scanner.nextDouble();
+
+        System.out.print("Enter the cost per minute for the second call (c2): ");
+        double c2 = scanner.nextDouble();
+        System.out.print("Enter the duration of the second call in minutes (t2): ");
+        double t2 = scanner.nextDouble();
+
+        System.out.print("Enter the cost per minute for the third call (c3): ");
+        double c3 = scanner.nextDouble();
+        System.out.print("Enter the duration of the third call in minutes (t3): ");
+        double t3 = scanner.nextDouble();
+
+        double cost1 = c1 * t1;
+        double cost2 = c2 * t2;
+        double cost3 = c3 * t3;
+
+        double totalCost = cost1 + cost2 + cost3;
+
+        System.out.println("Cost of the first call: " + cost1);
+        System.out.println("Cost of the second call: " + cost2);
+        System.out.println("Cost of the third call: " + cost3);
+        System.out.println("Total cost of all calls: " + totalCost);
+
+
+        scanner.close();
+
+
+    }
 }
+
